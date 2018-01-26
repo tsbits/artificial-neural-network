2e8ab7d488a68b490c1a92fdbff1f97e2b8f9ede
tsbits <desuko@gmail.com>
2018-01-26 15:19:44 +0100

    Predictor exemple looks to work...

---
+++ _code/CLDL_predictiveConverterEurosDollars/README.md
@@ -0,0 +1,7 @@
+#CLDL 001
+## Euros to Dollars linear predictive converter
+
+This program :
+• Generate a random convertion rate to get Dollars from Euro
+• Generate a training data set containing € -> $ pairs using the randomely getted convertion rate
+• "Train" the thing by iterating through all value a certain amount of times and try to find the best c
--- _code/CLDL_predictiveConverterEurosDollars/index.js
+++ _code/CLDL_predictiveConverterEurosDollars/index.js
@@ -1,1 +1,99 @@
-console.log('Hello NN CLDL');
+///////////////
+// VARIABLES //
+///////////////
+
+// C -> Coef. of multiplication to find to get $ from €
+// NB: The good C is 1.24136 (according to Google convert tool the 25/01/18)
+var c = Math.random()*5000; 
+// A set of validated data to train the "neuron" ?...
+var trainingDataSet;
+
+
+
+///////////////
+// FUNCTIONS //
+///////////////
+
+// Func • That function generate a random set of € / $ pair
+// Args length:uint (The number of pair to generate)
+// Args coefEuroDollars:Number (The multiplyer that gives $ from €)
+function generateDataSetEuroDollars( length, coefEuroDollars ){
+	var generatedDataSet = [];
+
+	for( var i = 0; i < length; i++ ){
+		var euro = Math.random() * 100;
+		generatedDataSet.push( [ euro, euro * coefEuroDollars ] );
+	}
+
+	return generatedDataSet;
+}
+
+// Iterate through the trainingDataSet "trials" time, trying to find the best C to get $ from €
+function train( trials ){
+ 	// Giving a random value to C to start the training
+	var trainingC = 15;
+	var bestErrorRatio = 1000000;
+	var ajustStep = 0.1;
+
+	for( var i = 0; i < trials; i++ ){
+		for( var j = 0; j < trainingDataSet.length; j++ ){
+			var guessedDollars = trainingDataSet[j][0] * trainingC;
+			var calculatedError = guessedDollars - trainingDataSet[j][1];
+			var errorRatio = guessedDollars / trainingDataSet[j][1];
+
+			if( Math.abs(errorRatio) < bestErrorRatio ){
+				bestErrorRatio = Math.abs(errorRatio);
+			}
+			else{
+				if( calculatedError < 0 ){
+					trainingC += ajustStep * errorRatio;
+				}
+				else{
+					trainingC -= ajustStep * errorRatio;
+				}
+			}
+
+			console.log('Iteration #' + i + ' on item #' + j + ' : € -> ' + trainingDataSet[j][0] + ' • $ -> ' + trainingDataSet[j][1] + ' • Guessed $ -> ' + guessedDollars + ' • Error -> ' + calculatedError + ' • Current trainingC -> ' + trainingC);
+		}
+
+
+	}
+
+	return trainingC;
+}
+
+function predict(){
+
+}
+
+
+
+/////////////
+// PROGRAM //
+/////////////
+
+console.log('€ to $ converter : started');
+console.log('- - - - - - - - - - - - - -');
+console.log('Training data set generation : started');
+console.log('- - - - - - - - - - - - - -');
+// trainingDataSet = generateDataSetEuroDollars(1, 1.24136);
+trainingDataSet = generateDataSetEuroDollars(100, c);
+console.log('Training data set generation : over');
+console.log('- - - - - - - - - - - - - -');
+console.log('Datas length :');
+console.log( trainingDataSet.length );
+console.log('- - - - - - - - - - - - - -');
+console.log('Starting the training');
+console.log('- - - - - - - - - - - - - -');
+var t = train(10000);
+console.log('- - - - - - - - - - - - - -');
+console.log('BestC -> ' + t + ' • Randomely selected convertion rate to generate the set : ' + c);
+
+
+
+
+
+
+
+
+
